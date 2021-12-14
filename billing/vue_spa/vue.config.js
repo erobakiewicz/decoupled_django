7313e1bf5825d6ee6486c712b363143c134193a0
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2021-12-14 19:05:56 +0100

    vue

---
+++ billing/vue_spa/vue.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+module.exports = {
+    publicPath: process.env.VUE_APP_STATIC_URL,
+    outputDir: path.resolve(__dirname, "../static", "billing"),
+    indexPath: path.resolve(__dirname, "../templates/", "billing", "index.html"),
+};
+
+// omitted
+module.exports = {
+    // omitted
+    devServer: {
+        proxy: "http://localhost:8000"
+    }
+};
