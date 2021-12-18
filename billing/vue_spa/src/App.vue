a143a88b27ce08a7764ba1db5b3ad7aebbff4947
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2021-12-18 16:29:47 +0100

    finished Vue app

--- billing/vue_spa/src/App.vue
+++ billing/vue_spa/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <InvoiceCreate />
+    <InvoiceCreate/>
   </div>
 </template>
 
@@ -10,18 +10,7 @@ import InvoiceCreate from "@/components/InvoiceCreate";
 export default {
   name: "App",
   components: {
-    InvoiceCreate,
-  },
+    InvoiceCreate
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- billing/vue_spa/src/components/InvoiceCreate.vue
+++ billing/vue_spa/src/components/InvoiceCreate.vue
@@ -9,6 +9,7 @@
             <select id="user" name="user" required>
               <option value="--">--</option>
               <option v-for="user in users" :key="user.email" :value="user.id">
+                {{ user.name }} - {{ user.email }}
               </option>
             </select>
           </div>
@@ -75,19 +76,25 @@ export default {
   },
   methods: {
     handleSubmit: function (event) {
-      // eslint-disable-next-line no-unused-vars
       const formData = new FormData(event.target);
-      // TODO - build the request body
-      const data = {};
+      const data = Object.fromEntries(formData);
+      data.items = [
+        {
+          quantity: formData.get("quantity"),
+          description: formData.get("description"),
+          price: formData.get("price"),
+          taxed: Boolean(formData.get("taxed"))
+        }
+      ];
       fetch("/billing/api/invoices/", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
       })
-          .then((response) => {
+          .then(response => {
             if (!response.ok) throw Error(response.statusText);
-          return response.json();
-        })
+            return response.json();
+          })
           .then(json => {
             console.log(json);
           })
@@ -105,5 +112,104 @@ export default {
         });
   }
 };
-
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&family=Nunito:wght@300;400&display=swap");
+
+h2 {
+  /*font-family: "Balsamiq Sans", cursive;*/
+  font-size: 2em;
+  margin-bottom: 2em;
+}
+
+.container {
+  font-family: "Nunito", sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+  max-width: 900px;
+  margin: auto;
+}
+
+.form {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.form__field {
+  margin-bottom: 2em;
+}
+
+.form__field label {
+  display: block;
+  margin-bottom: 0.5em;
+}
+
+.form__field input,
+select {
+  height: 30px;
+  margin-bottom: 1em;
+  margin-right: 1em;
+}
+
+.form__field select {
+  font-size: 0.6em;
+}
+
+.form__field input[type="date"] {
+  font-size: 0.6em;
+}
+
+.form__field select {
+}
+
+.form__field input {
+}
+
+#quantity {
+  width: 30px;
+}
+
+#description {
+}
+
+.form__aside {
+  display: flex;
+  flex-direction: column;
+}
+
+.form__main {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  flex: 1 0 0;
+}
+
+@media screen and (min-width: 960px) {
+  .form__main {
+    flex-wrap: nowrap;
+  }
+
+  #description {
+    min-width: 400px;
+  }
+}
+
+.form__buttons {
+  float: right;
+}
+
+.form__buttons button {
+  border: 2px solid #222;
+  box-shadow: 5px 5px 0 #222;
+  font: inherit;
+  font-size: 16px;
+  background-color: transparent;
+  width: 150px;
+  height: 40px;
+  margin-right: 0.9em;
+  padding: 2px;
+}
+</style>
